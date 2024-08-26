96a85a21d071a690070797231d5eb0b8e0cf47bb
Celso Serrano Araujo <celso.sraujo@hotmail.comm>
2024-08-26 19:26:51 -0300

    correção

--- sw.js
+++ sw.js
@@ -1,27 +1,28 @@
-const cacheName = 'cartao-celso';
-var filesToCache = [
+const cacheName = 'cartao-celso-v1';
+const filesToCache = [
   './',
   './manifest.webmanifest',
   './index.html',
-  './style.css',
-  './main.js'
+  './css/style.css',
+  './script/main.js',
+  './image/fotoCelso.png'
 ];
 
-/* Inicia o Service Worker e armazene em cache o conteúdo do aplicativo*/
-self.addEventListener('install', function (e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function (cache) {
+// Instala o Service Worker e armazena em cache os arquivos
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
       return cache.addAll(filesToCache);
     })
   );
   self.skipWaiting();
 });
 
-/* Servidor de armazenamento de cache quando estiver offline*/
-self.addEventListener('fetch', function (e) {
-  e.respondWith(
-    caches.match(e.request).then(function (response) {
-      return response || fetch(e.request);
+// Serve os arquivos do cache quando estiver offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
